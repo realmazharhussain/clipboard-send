e43120bd2d36e3745e6fb060fb8cdaefcea2effd
Mazhar Hussain <mazhar.hussain@texense.io>
2025-04-21 20:26:03 +0500

    Initial implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,13 +13,12 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -30,16 +29,4 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/io/github/realmazharhussain/clipboardshare/MainActivity.kt
+++ app/src/main/java/io/github/realmazharhussain/clipboardshare/MainActivity.kt
@@ -1,20 +1,53 @@
 package io.github.realmazharhussain.clipboardshare
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.ClipboardManager
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.LinearLayout
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val view = LinearLayout(this).apply {
+            setBackgroundColor(Color.TRANSPARENT)
+            post {
+                onViewRendered()
+                finish()
+            }
         }
+        setContentView(view)
+    }
+
+    private fun onViewRendered() {
+        val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as? ClipboardManager ?: run {
+            showToast("Clipboard service not available")
+            return
+        }
+
+        val text = clipboardManager.primaryClip?.takeIf { it.itemCount > 0 }?.getItemAt(0)?.coerceToText(this)?.toString() ?: run {
+            showToast("Clipboard is empty")
+            return
+        }
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
+
+        val chooser: Intent = Intent.createChooser(intent, "Share via")
+
+        try {
+            startActivity(chooser)
+        } catch (_: ActivityNotFoundException) {
+            showToast("An internal error occured!")
+        }
+    }
+
+    private fun showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, length).show()
     }
 }
